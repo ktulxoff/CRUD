5f8e13597bfece38f22d3e185677101e78131da1
ktulxoff <74500367+ktulxoff@users.noreply.github.com>
2023-04-26 12:52:25 +0300

    smth-2

--- src/main/java/ru/ktulxoff/crud/Main.java
+++ src/main/java/ru/ktulxoff/crud/Main.java
@@ -2,6 +2,6 @@ package ru.ktulxoff.crud;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hello world?");
     }
 }
